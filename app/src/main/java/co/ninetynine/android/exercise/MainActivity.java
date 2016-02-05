12d02a895166d67b85a9a3c603959bc5681da2f4
Suresh Subramaniam <sureshs.592@gmail.com>
2016-02-05 14:47:04 +0800

    Initialising Timber logging

--- app/src/main/java/co/ninetynine/android/exercise/MainActivity.java
+++ app/src/main/java/co/ninetynine/android/exercise/MainActivity.java
@@ -2,11 +2,13 @@ package co.ninetynine.android.exercise;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Timber.plant(new Timber.DebugTree());
     setContentView(R.layout.activity_main);
   }
 }
