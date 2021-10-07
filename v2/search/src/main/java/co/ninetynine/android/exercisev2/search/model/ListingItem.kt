da8283b059adbc96e6ce356bf474f6365496f813
akqeel <akqeelnawaz@windowslive.com>
2021-10-07 12:36:21 +0800

    feat(search): add retrofit

--- v2/search/src/main/java/co/ninetynine/android/exercisev2/search/model/ListingItem.kt
+++ v2/search/src/main/java/co/ninetynine/android/exercisev2/search/model/ListingItem.kt
@@ -1,24 +1,40 @@
 package co.ninetynine.android.exercisev2.search.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ListingItem(
+    @SerializedName("address")
     val address: Address,
+    @SerializedName("attributes")
     val attributes: Attributes,
+    @SerializedName("category")
     val category: String,
+    @SerializedName("completed_at")
     val completedAt: String,
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("photo")
     val photoUrl: String,
+    @SerializedName("project_name")
     val projectName: String,
+    @SerializedName("tenure")
     val tenure: Int
 )
 
 data class Address(
+    @SerializedName("district")
     val district: String,
+    @SerializedName("street_name")
     val streetName: String,
 )
 
 data class Attributes(
+    @SerializedName("area_size")
     val areaSize: Int,
+    @SerializedName("bathrooms")
     val bathrooms: Int,
+    @SerializedName("bedrooms")
     val bedrooms: Int,
+    @SerializedName("price")
     val price: Int,
 )
