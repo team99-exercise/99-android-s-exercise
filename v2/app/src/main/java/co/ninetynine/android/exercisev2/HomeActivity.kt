10f993b2d71c75d925e570dfa588184ad3c758f0
akqeel <akqeelnawaz@windowslive.com>
2021-10-07 09:39:34 +0800

    feat(app): add home activity

---
+++ v2/app/src/main/java/co/ninetynine/android/exercisev2/HomeActivity.kt
@@ -0,0 +1,22 @@
+package co.ninetynine.android.exercisev2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.ninetynine.android.exercisev2.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        launchSearchActivity()
+    }
+
+    private fun launchSearchActivity() {
+        // TODO: Go to search activity
+    }
+
+}
