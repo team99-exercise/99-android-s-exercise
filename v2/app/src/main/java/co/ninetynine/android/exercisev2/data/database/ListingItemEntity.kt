d9e1302ac9f990fdf823ba111649f936c5ced478
Nicat Mahirson <nicatmahirson@gmail.com>
2022-01-21 18:03:26 +0400

    editing ListingItemEntity.kt

--- v2/app/src/main/java/co/ninetynine/android/exercisev2/data/database/ListingItemEntity.kt
+++ v2/app/src/main/java/co/ninetynine/android/exercisev2/data/database/ListingItemEntity.kt
@@ -1,10 +1,33 @@
 package co.ninetynine.android.exercisev2.data.database
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity
 class ListingItemEntity(
     @PrimaryKey
-    var listingId: Int
+    var listingId: Int,
+    val category: String,
+    val completedAt: String,
+    val photoUrl: String,
+    val projectName: String,
+    val tenure: Int,
+    @Embedded(prefix = "address")
+    val address: Address,
+    @Embedded(prefix = "attributes")
+    val attributes: Attributes,
+)
+
+class Address(
+    val district: String,
+    val streetName: String,
+)
+
+class Attributes(
+    val areaSize: Int,
+    val bathrooms: Int,
+    val bedrooms: Int,
+    val price: Int,
 )
