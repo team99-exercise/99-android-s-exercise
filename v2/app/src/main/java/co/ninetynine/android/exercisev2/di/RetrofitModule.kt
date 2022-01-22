6486806da21904cce0e56a7cc58d494a0257af17
Nicat Mahirson <nicatmahirson@gmail.com>
2022-01-22 16:28:56 +0400

    added logging interceptor

--- v2/app/src/main/java/co/ninetynine/android/exercisev2/di/RetrofitModule.kt
+++ v2/app/src/main/java/co/ninetynine/android/exercisev2/di/RetrofitModule.kt
@@ -5,6 +5,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -13,13 +15,31 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
 
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+
     @Provides
     @Singleton
     fun provideRetrofit(
-        gsonConverterFactory: GsonConverterFactory
+        gsonConverterFactory: GsonConverterFactory,
+        okHttpClient: OkHttpClient
     ) = Retrofit.Builder()
         .addConverterFactory(gsonConverterFactory)
         .baseUrl(Constants.BASE_URL)
+        .client(okHttpClient)
         .build()
 
     @Provides
