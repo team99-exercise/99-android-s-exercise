5e2da0a7c39c4670f55138a25b3235fd9ac3f7a3
Lin Min Phyo <myeiklin@gmail.com>
2022-10-01 10:02:52 +0700

    chore: implement fetch data from API

--- v2/search/src/main/java/co/ninetynine/android/exercisev2/search/data/repository/SearchRepository.kt
+++ v2/search/src/main/java/co/ninetynine/android/exercisev2/search/data/repository/SearchRepository.kt
@@ -6,16 +6,12 @@ import co.ninetynine.android.exercisev2.search.model.ListingItem
 import javax.inject.Inject
 
 class SearchRepository @Inject constructor(
-    var service: SearchService,
-    var database: AppDatabase
+    private val service: SearchService,
+    private val database: AppDatabase
 ) {
 
-    fun getSearchResults(): List<ListingItem> {
-        TODO()
-        // 1. "Return results from `database` if not empty
-        // 2. Else fetch results from `service`
-        // 3. Cache results in `database`
-        // 4. Return results
+    suspend fun getSearchResults(): List<ListingItem> {
+        return service.getSearchResults()
     }
 
 }
