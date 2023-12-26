d8227b4304d0dec8a1846403f7ed7dfa34fff40a
Shawn Baek <shawn@shawnbaek.com>
2023-12-26 15:36:04 +0900

    Remove project folder

--- v2/app/src/main/java/co/ninetynine/android/exercisev2/NinetyNineExerciseApp.kt
+++
@@ -1,7 +0,0 @@
-package co.ninetynine.android.exercisev2
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class NinetyNineExerciseApp : Application()
--- v2/search/src/main/java/co/ninetynine/android/exercisev2/search/ext/Context.kt
+++
@@ -1,8 +0,0 @@
-package co.ninetynine.android.exercisev2.search.ext
-
-import android.content.Context
-import android.view.LayoutInflater
-
-fun Context.getLayoutInflater() = getSystemService(
-    Context.LAYOUT_INFLATER_SERVICE
-) as LayoutInflater
