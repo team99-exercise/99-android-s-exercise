0fae2a24cba556cc5ba95a8339b004a9b6e99f47
Shawn Baek <shawn@shawnbaek.com>
2023-12-26 15:36:04 +0900

    Remove project folder

--- v2/app/src/main/java/co/ninetynine/android/exercisev2/data/database/AppDatabase.kt
+++
@@ -1,7 +0,0 @@
-package co.ninetynine.android.exercisev2.data.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [ListingItemEntity::class], version = 1)
-abstract class AppDatabase: RoomDatabase()
